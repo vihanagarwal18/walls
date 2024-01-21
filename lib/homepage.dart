0895b2857e614a54553635521574417ccdeab55b
Vihan Agarwal <vihanagarwal2003@gmail.com>
2024-01-22 02:19:26 +0530

    drawer commit 1�

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -7,6 +7,7 @@ import 'package:Walls/liked_images.dart';
 import 'firebase_Storage_services.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:flutter_advanced_drawer/flutter_advanced_drawer.dart';
 
 
 class Homepage extends StatefulWidget {
@@ -27,73 +28,136 @@ class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
     print(images_list);
-    return Scaffold(
-      appBar: AppBar(
-        title: Center(child: Text("hello")),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.favorite),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => LikedWallpapersPage()),
-              );
-            },
+    return AdvancedDrawer(
+      backdrop: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [Colors.blueGrey, Colors.blueGrey.withOpacity(0.2)],
           ),
-        ],
-      ),
-      body: MasonryGridView.builder(
-        //itemCount: 15,
-        itemCount: images_list.length,
-        gridDelegate: SliverSimpleGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
         ),
-        itemBuilder: (context, index) => GestureDetector(
-          onTap: () {
-            //_showFullScreenImage(context, (index + 1).toString() + '.png');
-            _showFullScreenImage(context, images_list[index].toString());
-          },
-          child: Padding(
-            padding: const EdgeInsets.all(5.0),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(18),
-              child: FutureBuilder(
-                //future: _getImage(context, (index + 1).toString() + '.png'),
-
-                future: _getImage(context, images_list[index].toString()),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.done &&
-                      snapshot.hasData) {
-                    return Column(
-                      children: [
-                        Container(
-                          width: MediaQuery.of(context).size.width / 1.2,
-                          height: MediaQuery.of(context).size.width / 1.2,
-                          child: snapshot.data,
+      ),
+      //controller: _advancedDrawerController,
+      disabledGestures: false,
+      animationCurve: Curves.easeInOut,
+      animationDuration: const Duration(milliseconds: 300),
+      animateChildDecoration: true,
+      rtlOpening: false,
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('About the App'),
+            ),
+            ListTile(
+              title: Row(
+                children: [
+                  Text('About the Developers'),
+                ],
+              ),
+            ),
+            ListTile(
+              title: Text('Liked Images'),
+              onTap: (){
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => LikedWallpapersPage()),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Center(child: Text("hello")),
+          leading: IconButton(
+            onPressed: _handleMenuButtonPressed,
+            icon: ValueListenableBuilder<AdvancedDrawerValue>(
+              valueListenable: _advancedDrawerController,
+              builder: (_, value, __) {
+                return AnimatedSwitcher(
+                  duration: Duration(milliseconds: 250),
+                  child: Icon(
+                    value.visible ? Icons.clear : Icons.menu,
+                    key: ValueKey<bool>(value.visible),
+                  ),
+                );
+              },
+            ),
+          ),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.favorite),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => LikedWallpapersPage()),
+                );
+              },
+            ),
+          ],
+        ),
+        body: MasonryGridView.builder(
+          //itemCount: 15,
+          itemCount: images_list.length,
+          gridDelegate: SliverSimpleGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+          ),
+          itemBuilder: (context, index) => GestureDetector(
+            onTap: () {
+              //_showFullScreenImage(context, (index + 1).toString() + '.png');
+              _showFullScreenImage(context, images_list[index].toString());
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(18),
+                child: FutureBuilder(
+                  //future: _getImage(context, (index + 1).toString() + '.png'),
+      
+                  future: _getImage(context, images_list[index].toString()),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.done &&
+                        snapshot.hasData) {
+                      return Column(
+                        children: [
+                          Container(
+                            width: MediaQuery.of(context).size.width / 1.2,
+                            height: MediaQuery.of(context).size.width / 1.2,
+                            child: snapshot.data,
+                          ),
+                        ],
+                      );
+                    } else if (snapshot.connectionState ==
+                        ConnectionState.waiting) {
+                      return Container(
+                        width: MediaQuery.of(context).size.width / 1.2,
+                        height: MediaQuery.of(context).size.width / 1.2,
+                        child: Center(
+                          child: CircularProgressIndicator(),
                         ),
-                      ],
-                    );
-                  } else if (snapshot.connectionState ==
-                      ConnectionState.waiting) {
-                    return Container(
-                      width: MediaQuery.of(context).size.width / 1.2,
-                      height: MediaQuery.of(context).size.width / 1.2,
-                      child: Center(
-                        child: CircularProgressIndicator(),
-                      ),
-                    );
-                  } else {
-                    return Container(
-                      color: Colors.red,
-                    );
-                  }
-                },
+                      );
+                    } else {
+                      return Container(
+                        color: Colors.red,
+                      );
+                    }
+                  },
+                ),
               ),
             ),
           ),
         ),
+        backgroundColor: Colors.green,
       ),
-      backgroundColor: Colors.green,
     );
   }
 
@@ -145,5 +209,10 @@ class _HomepageState extends State<Homepage> {
     } else {
       print("Failed to fetch data. Status code: ${response.statusCode}");
     }
+  }
+  void _handleMenuButtonPressed() {
+    // NOTICE: Manage Advanced Drawer state through the Controller.
+    // _advancedDrawerController.value = AdvancedDrawerValue.visible();
+    _advancedDrawerController.showDrawer();
   }
 }
